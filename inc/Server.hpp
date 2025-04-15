3f3f18baf5b2036140c3d90767ecd954f33dc7e3
aklimchu <aklimchu@student.hive.fi>
2025-04-15 11:05:37 +0300

    added class instance, added try-catch block

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -8,17 +8,42 @@
 // #include <errno.h>
 #include <unistd.h> // for close
 
+#define BACKLOG 10
+#define PORT 8888
+
 class Server {
 	public:
-		Server(void) = delete;
+		Server(void) = default;
 		Server(Server & src) = delete;
 
 		~Server(void) = default;
 
 		Server & operator=(Server & rhs) = delete;
 
-		static int initServer(void);
-		static int startServer(void);
+		void initServer(void);
+		void startServer(void);
+
+		class SocketError : public std::exception {
+			public:
+				virtual const char* what() const throw() {
+					return ("Error while creating a socket");
+				}
+		};
+
+		class BindingError : public std::exception {
+			public:
+				virtual const char* what() const throw() {
+					return ("Error in binding");
+				}
+		};
+
+		class ListeningError : public std::exception {
+			public:
+				virtual const char* what() const throw() {
+					return ("Error in listening");
+				}
+		};
 
 	private:
+		int sockfd;
 };
--- src/Server.cpp
+++ src/Server.cpp
@@ -1,54 +1,46 @@
 #include "Server.hpp"
 
-//Comment
-
-int Server::initServer(void) {
-	int sockfd;
+void Server::initServer(void) {
 	struct sockaddr_in sockaddr;
 
-	if (((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)) {
-		std::cout << "Comment made by Johan" << std::endl;
-		return(1);
-	}
+	if (((this->sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0))
+		throw SocketError();
 
 	std::memset(&sockaddr, 0, sizeof(sockaddr));
 	sockaddr.sin_family = AF_INET;
-	sockaddr.sin_port = htons(8888); // can take from argv
+	sockaddr.sin_port = htons(PORT); // can take from argv
 	sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	if (((bind(sockfd, (struct sockaddr *) &sockaddr, sizeof(sockaddr))) < 0)) {
-		std::cout << "Error in binding" << std::endl;
-		return(1);
+	if (((bind(this->sockfd, (struct sockaddr *) &sockaddr, sizeof(sockaddr))) < 0)) {
+		throw BindingError();
 	}
 
-	if ((listen(sockfd, 10)) < 0) {
-		std::cout << "Error in listening" << std::endl;
-		return(1);
+	if ((listen(this->sockfd, BACKLOG)) < 0) {
+		throw ListeningError();
 	}
 
-	while (1)  {
+	//while (1)  {
 		//accept or poll
 		//or poll always
-	}
+	//}
 
-	close(sockfd);
-
-	return 0;
+	close(this->sockfd);
 }
 
-int Server::startServer(void) {
-	return 0;
-}
+void Server::startServer(void) {}
 
 int main (int argc, char *argv[]) {
+	Server irc_server;
+		
 	/* if (argc < 3 || argc > 3)
 		std::cout << "Wrong number of arguments" << std::endl; */
 	(void)argv;
 	(void)argc;
-	if (Server::initServer() == 1) {
-		return 1;
-	};
-	if (Server::startServer() == 1) {
-		return 1;
-	};
+	try {
+		irc_server.initServer();
+		irc_server.startServer();
+	}
+	catch (std::exception & e) {
+		std::cerr << e.what() << std::endl;
+	}
 }
