5909240a95b2b36954ce509e1fb1ed59f5b5a5eb
Johan Niemi <jniemi@student.hive.fi>
2025-04-16 11:28:32 +0300

    continued to implement Client class

--- inc/Client.hpp
+++ inc/Client.hpp
@@ -3,12 +3,18 @@
 
 #include <string>
 
+
 class	Client
 {
 	private:
 		int			_fd;
 		std::string	_username;
 		std::string	_nickname;
+		std::string	_realname;
+		std::string _buffer;
+		bool		_registered;
+		bool		_passwdOK;
+
 	
 	public:
 		Client();
@@ -18,6 +24,19 @@ class	Client
 		int					getFd() const;
 		const std::string	&getUsername() const;
 		const std::string	&getNickname() const;
+		const std::string	&getRealname() const;
+		const std::string	&getBuffer() const;
+		bool				isRegistered() const;
+		bool				isPasswdOK() const;
+
+		void	setUsername(const std::string &str);
+		void	setNickname(const std::string &str);
+		void	setRealname(const std::string &str);
+		void	setBuffer(const std::string &str);
+		void	appendBuffer(const std::string &str);
+		void	clearBuffer();
+		void	setRegistered(bool value);
+		void	setPasswdOK(bool value);
 
 };
 
--- src/Client.cpp
+++ src/Client.cpp
@@ -1,10 +1,10 @@
 #include "Client.hpp"
 
-Client::Client()
+Client::Client() : _fd(-1), _registered(false), _passwdOK(false)
 {
 }
 
-Client::Client(int fd) : _fd(fd)
+Client::Client(int fd) : _fd(fd), _registered(false), _passwdOK(false)
 {
 }
 
@@ -12,6 +12,7 @@ Client::~Client()
 {
 }
 
+//GETTERS:
 int	Client::getFd() const
 {
 	return (this->_fd);
@@ -25,4 +26,65 @@ const std::string	&Client::getUsername() const
 const std::string	&Client::getNickname() const
 {
 	return (this->_nickname);
+}
+
+const std::string	&Client::getRealname() const
+{
+	return (this->_realname);
+}
+
+const std::string	&Client::getBuffer() const
+{
+	return (this->_buffer);
+}
+
+bool	Client::isRegistered() const
+{
+	return (this->_registered);
+}
+
+bool	Client::isPasswdOK() const
+{
+	return (this->_passwdOK);
+}
+
+//SETTERS:
+void	Client::setUsername(const std::string &str)
+{
+	this->_username = str;
+}
+
+void	Client::setNickname(const std::string &str)
+{
+	this->_nickname = str;
+}
+
+void	Client::setRealname(const std::string &str)
+{
+	this->_realname = str;
+}
+
+void	Client::setBuffer(const std::string &str)
+{
+	this->_buffer = str;
+}
+
+void	Client::appendBuffer(const std::string &str)
+{
+	this->_buffer += str;
+}
+
+void	Client::clearBuffer()
+{
+	this->_buffer.clear();
+}
+
+void	Client::setRegistered(bool value)
+{
+	this->_registered = value;
+}
+
+void	Client::setPasswdOK(bool value)
+{
+	this->_passwdOK = value;
 }
