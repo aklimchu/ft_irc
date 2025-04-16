652861e1d52afa0fffc616113641af32f9672193
aklimchu <aklimchu@student.hive.fi>
2025-04-16 09:03:23 +0300

    changed names of attributes

--- src/Message.cpp
+++ src/Message.cpp
@@ -3,14 +3,14 @@
 //--------------------------------Constructors--------------------------------//
 
 Message::Message(std::string sender, std::string receiver, std::string command_called) : \
-	sender(sender), receiver(receiver), command_called(command_called) {}
+	_sender(sender), _receiver(receiver), _command_called(command_called) {}
 
 //-------------------------------Member functions------------------------------//
 
 void Message::callCommand(void) const {
 	//checking user permissions before executing the command?
-	for (int i = 0; i < sizeof(this->functions); i++) {
-		if (this->command_called == this->function_names[i]) // how about MODE + flag?
-			this->functions[i]();
+	for (int i = 0; i < sizeof(this->_functions); i++) {
+		if (this->_command_called == this->_function_names[i]) // how about MODE + flag?
+			this->_functions[i]();
 	}
 }
